41e81e85e70fd9fc839a94ae0c674330ab3760c6
Adel Saad <adelalderby@gmail.com>
2021-03-31 07:42:06 +0300

    preparing project

---
+++ lib/Modules/category.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Category {
+// props ...
+  final String id;
+  final String title;
+  final Color color;
+
+// constr ...
+  Category(
+      {@required this.id, @required this.title, this.color = Colors.orange});
+
+// methods ...
+
+}
