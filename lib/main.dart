bafc4e6cc32ce873b395611eadd9c4c23421dab4
Adel Saad <adelalderby@gmail.com>
2021-03-31 06:49:30 +0300

    meal_app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/Views/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomePage.id,
+      routes: {HomePage.id: (context) => HomePage()},
+    );
+  }
+}
